fa6063cada89ccbb6c76c3048fc2144b9a71fc15
Aditya Yadav <217367477+adityayadav176@users.noreply.github.com>
2026-05-21 22:56:22 +0530

    Add changeIsDefault controller and route for toggling default address status with validation and ownership checks

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -201,7 +201,54 @@ const updateAddress = asyncHandler(async (req, res) => {
     );
 });
 
+const changeIsDefault = asyncHandler(async (req, res) => {
+    const userId = req.user?._id
+
+    if(!userId) {
+        throw new ApiError(401, "Unauthorized Access Denied");
+    }
+
+    const {addressId} = req.params
+
+    if(!addressId || !mongoose.isValidObjectId(addressId)) {
+        throw new ApiError(400, "Invalid AddressId");
+    }
+
+    const existedAddress = await Address.findById(addressId);
+
+    if(!existedAddress) {
+        throw new ApiError(404, "Address Not Found");
+    }
+
+    if (existedAddress.user.toString() !== userId.toString()) {
+            throw new ApiError(403, "You Are Not Allowed To Perform This Task");
+    }
+
+    const newAddress = await Address.findByIdAndUpdate(
+        addressId,
+        {
+            $set: {
+                isDefault: !existedAddress.isDefault
+            },
+        },
+        {
+            new: true
+        }
+    )
+
+   if(!newAddress) {
+           throw new ApiError(500, "Something Went Wrong While Updating address ChangeIsDefault");
+    }
+
+   return res
+   .status(200)
+   .json(
+    new ApiResponse(200, newAddress, `Address is now ${!existedAddress.isDefault ? "default" : "not default"}`)
+   )
+})
+
 export {
     CreateAddress,
     updateAddress,
+    changeIsDefault,
 }
--- src/routes/address.routes.js
+++ src/routes/address.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { CreateAddress, updateAddress } from "../controllers/address.controller.js";
+import { changeIsDefault, CreateAddress, updateAddress } from "../controllers/address.controller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 
 const router = Router()
@@ -7,4 +7,5 @@ const router = Router()
 
 router.route("/createAddress").post(verifyJwt ,CreateAddress);
 router.route("/updateAddress/:addressId").patch(verifyJwt, updateAddress);
+router.route("/changeIsDefault/:addressId").patch(verifyJwt, changeIsDefault);
 export default router;
