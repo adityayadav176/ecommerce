45302536acb98e4f8743d1dd6b5f5d3a36231f19
Aditya Yadav <217367477+adityayadav176@users.noreply.github.com>
2026-05-23 09:58:19 +0530

    Add JWT verification middleware to /okk route

--- src/routes/okk.routes.js
+++ src/routes/okk.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { okkController } from "../controllers/everything.ok.controller.js";
+import { verifyJwt} from "../middleware/auth.middleware.js"
 
 const router = Router();
 
-router.route("/okk").get(okkController);
+router.route("/okk").get(verifyJwt, okkController);
 export default router;
