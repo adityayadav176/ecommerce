0f9c973b6b2acc3a4956e6e120792a78638b3535
Aditya Yadav <217367477+adityayadav176@users.noreply.github.com>
2026-05-29 09:28:50 +0530

    Add Updated Notifcation Controller

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -35,6 +35,46 @@ const createNotification = asyncHandler((req, res) => {
 
 
 
+const updateNotification = asyncHandler((req, res) => {
+    const {title, message, type} = req.body
+
+    if([title, message, type].some(item => item == null || item == undefined || item == ""));
+
+    const userId = req.user._id;
+
+    if(!userId) {
+        throw new ApiError(401, "Unauthorized Access Denied");
+    }
+
+    const notificationId = req.params;
+
+    if(!notificationId || !mongoose.isValidObjectId(notificationId)) {
+        throw new ApiError(400, "Invalid Notification Id");
+    }
+
+    const ExistingNotification = Notification.findByIdAndUpdate(
+        notificationId,
+        {
+            title,
+            message,
+            type
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!ExistingNotification) {
+        throw new ApiError(404, "Notification Not Found Or Updated");
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200, ExistingNotification, "Notification Updated Successfully")
+    )
+})
+
 export {
-    createNotification
+    createNotification,
+    updateNotification
 }
